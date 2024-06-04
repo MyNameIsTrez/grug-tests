848d1cfc3fabc876f76b85afcd71d1ab2dbf0add
MyNameIsTrez <welfje@gmail.com>
2024-06-04 21:36:14 +0200

    Add tests_ok/minimal/

--- tests_ok/minimal/input.s
+++ tests_ok/minimal/input.s
@@ -1,27 +1,5 @@
-global define
-
 global define_type
-
-global get_globals_struct_size
-global init_globals_struct
 
 section .data
 
-define:
-	db 42
-
 define_type: db "entity", 0
-
-section .text
-
-get_globals_struct_size:
-	mov rax, 0
-	ret
-
-; TODO: Translate this:
-; void init_globals_struct(void *globals_struct) {
-;     memcpy(globals_struct, &(struct globals){
-;     }, sizeof(struct globals));
-; }
-init_globals_struct:
-	ret
