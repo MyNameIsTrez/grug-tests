972f0da2979fcb1b9f928c0c1fdfacc3b75cbbca
MyNameIsTrez <welfje@gmail.com>
2024-06-05 20:23:53 +0200

    Pass tests_ok/minimal

--- tests.sh
+++ tests.sh
@@ -23,14 +23,14 @@ error_diff_path="results/error_diff_path.txt"
 run_test_err() {
 	local dir=$1
 
-	local grug_path="$dir/input.grug"
+	local grug_path=$dir"input.grug"
 
 	printf "Running $dir...\n"
 
 	./a.out $grug_path $dll_path >$grug_output_path 2>&1
 	local grug_exit_status=$?
 
-	local expected_error_path="$dir/expected_error.txt"
+	local expected_error_path=$dir"expected_error.txt"
 
 	diff $grug_output_path $expected_error_path >$error_diff_path
 
@@ -59,11 +59,11 @@ run_tests_err() {
 run_test_ok() {
 	local dir=$1
 
-	local nasm_path="$dir/input.s"
+	local nasm_path=$dir"input.s"
 
 	nasm -f elf64 $nasm_path -o $expected_o_path && ld -shared --hash-style=sysv $expected_o_path -o $expected_dll_path && rm $expected_o_path
 
-	local grug_path="$dir/input.grug"
+	local grug_path=$dir"input.grug"
 
 	printf "Running $dir...\n"
 
--- tests_ok/minimal/input.s
+++ tests_ok/minimal/input.s
@@ -1,5 +1,13 @@
 global define_type
 
+global get_globals_struct_size
+
 section .data
 
 define_type: db "entity", 0
+
+section .text
+
+get_globals_struct_size:
+	mov rax, 8
+	ret
