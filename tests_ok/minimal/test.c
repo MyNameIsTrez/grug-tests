dc2671c8da63cfba15ffa8e7b46121b0a2a5ae17
MyNameIsTrez <welfje@gmail.com>
2024-06-06 22:32:30 +0200

    Use entity struct in tests_ok/minimal/

--- tests_ok/minimal/test.c
+++ tests_ok/minimal/test.c
@@ -8,6 +8,10 @@
 typedef size_t (*get_globals_struct_size)(void);
 typedef void (*init_globals_struct)(void *globals_struct);
 
+struct entity {
+	uint64_t a;
+};
+
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wpedantic"
 struct globals {
@@ -15,12 +19,12 @@ struct globals {
 #pragma GCC diagnostic pop
 
 void *get(void *handle, char *label) {
-    void *p = dlsym(handle, label);
-    if (!p) {
-        fprintf(stderr, "dlsym: %s", dlerror());
-        exit(EXIT_FAILURE);
-    }
-    return p;
+	void *p = dlsym(handle, label);
+	if (!p) {
+		fprintf(stderr, "dlsym: %s", dlerror());
+		exit(EXIT_FAILURE);
+	}
+	return p;
 }
 
 int main(int argc, char *argv[]) {
@@ -31,20 +35,24 @@ int main(int argc, char *argv[]) {
 
 	char *so_path = argv[1];
 
-    void *handle = dlopen(so_path, RTLD_NOW);
-    if (!handle) {
-        fprintf(stderr, "dlopen: %s", dlerror());
-        exit(EXIT_FAILURE);
-    }
+	void *handle = dlopen(so_path, RTLD_NOW);
+	if (!handle) {
+		fprintf(stderr, "dlopen: %s", dlerror());
+		exit(EXIT_FAILURE);
+	}
 
-    assert(strcmp(get(handle, "define_type"), "entity") == 0);
-    assert(*(uint64_t *)get(handle, "define") == 42);
+	assert(strcmp(get(handle, "define_type"), "entity") == 0);
 
-    #pragma GCC diagnostic push
-    #pragma GCC diagnostic ignored "-Wpedantic"
-    assert(((get_globals_struct_size)get(handle, "get_globals_struct_size"))() == 0);
+	struct entity e = *(struct entity *)get(handle, "define");
+	assert(e.a == 42);
 
-    struct globals g;
-    ((init_globals_struct)get(handle, "init_globals_struct"))(&g);
-    #pragma GCC diagnostic pop
+	#pragma GCC diagnostic push
+	#pragma GCC diagnostic ignored "-Wpedantic"
+	get_globals_struct_size f = get(handle, "get_globals_struct_size");
+	size_t r = f();
+	assert(r == 0);
+
+	struct globals g;
+	((init_globals_struct)get(handle, "init_globals_struct"))(&g);
+	#pragma GCC diagnostic pop
 }
