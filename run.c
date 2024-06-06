8930aab55e578584f2c6cf46debbd7996658c105
MyNameIsTrez <welfje@gmail.com>
2024-06-07 00:23:23 +0200

    Pass variables, structs, and fn metadata to grug_init()

--- run.c
+++ run.c
@@ -13,7 +13,26 @@ int main(int argc, char *argv[]) {
 	char *dll_path = argv[2];
 	char *c_path = argv[3];
 
-	grug_init((grug_mod_types_t){.a=42});
+	grug_init((grug_mod_types_t){
+		.variables=(grug_variable_t[]){
+			{ .name="i32", .size=4 },
+			{ .name="string", .size=8 },
+			{0}
+		},
+		.structs=(grug_struct_t[]){
+			{
+				.name="entity", .fields=(grug_variable_t[]){
+					{ .name="a", .size=8 },
+					{0}
+				}
+			},
+			{0}
+		},
+		.fns=(grug_fn_t[]){
+			{ .name="foo" },
+			{0}
+		}
+	});
 
 	if (grug_test_regenerate_dll(grug_path, dll_path, c_path)) {
 		fprintf(stderr, "%s\n", grug_error.msg);
