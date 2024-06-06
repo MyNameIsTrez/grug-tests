b443280f36811964792a2b3945da11c6875e4142
MyNameIsTrez <welfje@gmail.com>
2024-06-06 23:44:04 +0200

    Use grug_init()

--- run.c
+++ run.c
@@ -13,6 +13,8 @@ int main(int argc, char *argv[]) {
 	char *dll_path = argv[2];
 	char *c_path = argv[3];
 
+	grug_init((grug_mod_types_t){.a=42});
+
 	if (grug_test_regenerate_dll(grug_path, dll_path, c_path)) {
 		fprintf(stderr, "%s\n", grug_error.msg);
 		exit(EXIT_FAILURE);
