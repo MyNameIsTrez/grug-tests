b238dc41691cd7ae8ab708d29ce5d19c36eb043b
MyNameIsTrez <welfje@gmail.com>
2024-07-06 09:16:09 +0200

    Add tests_ok/define_containing_string

---
+++ tests_ok/define_containing_string/input.s
@@ -0,0 +1,33 @@
+global define_type
+global globals_size
+global strings
+
+global define
+global init_globals
+
+global on_fns
+
+section .data
+
+define_type: db "k", 0
+
+globals_size: dq 0
+
+strings:
+	db "foo", 0
+
+on_fns:
+	dq 0
+
+section .text
+
+extern define_k
+
+define:
+	mov rdi, 42
+	lea rsi, [rel strings+0]
+	call define_k wrt ..plt
+	ret
+
+init_globals:
+	ret
