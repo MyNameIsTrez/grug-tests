69c4d475170fe227450d0d0eb009979a94c5fe4b
MyNameIsTrez <welfje@gmail.com>
2024-07-08 17:35:14 +0200

    Add tests_ok/on_fn_calling_game_fn_nothing

---
+++ tests_ok/on_fn_calling_game_fn_nothing/test.c
@@ -0,0 +1,77 @@
+#include "grug.h"
+
+#include <assert.h>
+#include <dlfcn.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void on_a(void);
+
+struct h {
+	int32_t x;
+};
+
+struct my_on_fns {
+	typeof(on_a) *a;
+};
+
+static struct h h_definition;
+
+void define_h(int32_t x) {
+	h_definition = (struct h){
+		.x = x,
+	};
+}
+
+bool fn_nothing_was_called = false;
+void nothing(void) {
+	fn_nothing_was_called = true;
+}
+
+static void *get(void *handle, char *label) {
+	void *p = dlsym(handle, label);
+	if (!p) {
+		fprintf(stderr, "dlsym: %s\n", dlerror());
+		exit(EXIT_FAILURE);
+	}
+	return p;
+}
+
+int main(int argc, char *argv[]) {
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <so_path>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	char *so_path = argv[1];
+
+	void *handle = dlopen(so_path, RTLD_NOW);
+	if (!handle) {
+		fprintf(stderr, "dlopen: %s\n", dlerror());
+		exit(EXIT_FAILURE);
+	}
+
+	assert(strcmp(get(handle, "define_type"), "h") == 0);
+
+	#pragma GCC diagnostic push
+	#pragma GCC diagnostic ignored "-Wpedantic"
+	grug_define_fn_t define = get(handle, "define");
+	define();
+	assert(h_definition.x == 42);
+
+	size_t globals_size = *(size_t *)get(handle, "globals_size");
+	assert(globals_size == 0);
+
+	void *g = malloc(globals_size);
+	grug_init_globals_fn_t init_globals = get(handle, "init_globals");
+	init_globals(g);
+	free(g);
+
+	struct my_on_fns *on_fns = get(handle, "on_fns");
+	assert(!fn_nothing_was_called);
+	on_fns->a();
+	assert(fn_nothing_was_called);
+	#pragma GCC diagnostic pop
+}
