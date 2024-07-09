5560c484692f049990e8983b47976d8a0898002a
MyNameIsTrez <welfje@gmail.com>
2024-07-09 21:18:06 +0200

    Add tests_ok/on_fn_calling_game_fn_nothing_twice

---
+++ tests_ok/on_fn_calling_game_fn_nothing_twice/input.s
@@ -0,0 +1,33 @@
+section .data
+
+global define_type
+define_type: db "h", 0
+
+align 8
+global globals_size
+globals_size: dq 0
+
+global on_fns
+on_fns:
+	dq on_a
+
+section .text
+
+extern define_h
+extern nothing
+
+global define
+define:
+	mov rdi, 42
+	call define_h wrt ..plt
+	ret
+
+global init_globals
+init_globals:
+	ret
+
+global on_a
+on_a:
+	call nothing wrt ..plt
+	call nothing wrt ..plt
+	ret
