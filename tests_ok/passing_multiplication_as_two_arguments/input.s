e840d7ee087868997d33ac2f3a9080ba7f96f581
MyNameIsTrez <welfje@gmail.com>
2024-07-18 01:30:11 +0200

    Add tests_ok/passing_multiplication_as_two_arguments

---
+++ tests_ok/passing_multiplication_as_two_arguments/input.s
@@ -0,0 +1,47 @@
+section .data
+
+global define_type
+define_type: db "d", 0
+
+align 8
+global globals_size
+globals_size: dq 0
+
+global on_fns
+on_fns:
+	dq on_a
+
+section .text
+
+extern define_d
+extern max
+
+global define
+define:
+	call define_d wrt ..plt
+	ret
+
+global init_globals
+init_globals:
+	ret
+
+global on_a
+on_a:
+	mov rax, 2
+	push rax
+	mov rax, 3
+	pop rbx
+	imul rax, rbx ; rax = 3 * 2
+	push rax
+
+	mov rax, 4
+	push rax
+	mov rax, 5
+	pop rbx
+	imul rax, rbx ; rax = 5 + 4
+	push rax
+
+	pop rsi
+	pop rdi
+	call max wrt ..plt
+	ret
