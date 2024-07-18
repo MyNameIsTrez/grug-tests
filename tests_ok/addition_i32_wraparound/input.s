825fa3fb15ef200eec8d623a71d018ecded13e71
MyNameIsTrez <welfje@gmail.com>
2024-07-18 20:11:49 +0200

    Add i32 and i64 addition wraparound tests

---
+++ tests_ok/addition_i32_wraparound/input.s
@@ -0,0 +1,39 @@
+section .data
+
+global define_type
+define_type: db "d", 0
+
+align 8
+global globals_size
+globals_size: dq 0
+
+global on_fns
+on_fns:
+	dq on_a
+
+section .text
+
+extern define_d
+extern initialize
+
+global define
+define:
+	call define_d wrt ..plt
+	ret
+
+global init_globals
+init_globals:
+	ret
+
+global on_a
+on_a:
+	mov rax, 2147483647
+	push rax
+	mov rax, 1
+	pop rbx
+	add rax, rbx
+	push rax
+
+	pop rdi
+	call initialize wrt ..plt
+	ret
