58659e94668d7141119f9c332e07c6d30088887c
MyNameIsTrez <welfje@gmail.com>
2024-07-18 19:43:46 +0200

    Add tests

---
+++ tests_ok/i32_min/input.s
@@ -0,0 +1,39 @@
+section .data
+
+global define_type
+define_type: db "d", 0
+
+align 8
+global globals_size
+globals_size: dq 0
+
+global on_fns
+on_fns:
+	dq on_a
+
+section .text
+
+extern define_d
+extern initialize
+
+global define
+define:
+	call define_d wrt ..plt
+	ret
+
+global init_globals
+init_globals:
+	ret
+
+global on_a
+on_a:
+	mov rax, 1
+	push rax
+	mov rax, -2147483647
+	pop rbx
+	sub rax, rbx
+	push rax
+
+	pop rdi
+	call initialize wrt ..plt
+	ret
