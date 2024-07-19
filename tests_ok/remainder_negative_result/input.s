c7f4689a7bb69878910a5c3d7f13a089ca989475
MyNameIsTrez <welfje@gmail.com>
2024-07-19 02:25:47 +0200

    Add remainder operator tests

---
+++ tests_ok/remainder_negative_result/input.s
@@ -0,0 +1,41 @@
+section .data
+
+global define_type
+define_type: db "d", 0
+
+align 8
+global globals_size
+globals_size: dq 0
+
+global on_fns
+on_fns:
+	dq on_a
+
+section .text
+
+extern define_d
+extern initialize
+
+global define
+define:
+	call define_d wrt ..plt
+	ret
+
+global init_globals
+init_globals:
+	ret
+
+global on_a
+on_a:
+	mov rax, 2
+	push rax
+	mov rax, -5
+	pop rbx
+	cqo
+	idiv rbx
+	mov rax, rdx
+	push rax
+
+	pop rdi
+	call initialize wrt ..plt
+	ret
