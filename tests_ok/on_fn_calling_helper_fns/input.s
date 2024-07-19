9739c281b99541f455915e7f3f7a2fee15e3cebc
MyNameIsTrez <welfje@gmail.com>
2024-07-20 00:13:27 +0200

    Pass tests_ok/on_fn_calling_helper_fns

---
+++ tests_ok/on_fn_calling_helper_fns/input.s
@@ -0,0 +1,46 @@
+section .data
+
+global define_type
+define_type: db "d", 0
+
+align 8
+global globals_size
+globals_size: dq 0
+
+global on_fns
+on_fns:
+	dq on_a
+
+section .text
+
+extern define_d
+extern nothing
+extern initialize
+
+global define
+define:
+	call define_d wrt ..plt
+	ret
+
+global init_globals
+init_globals:
+	ret
+
+global on_a
+on_a:
+	call foo
+	call bar
+	ret
+
+global foo
+foo:
+	call nothing wrt ..plt
+	ret
+
+global bar
+bar:
+	mov eax, 42
+	push rax
+	pop rdi,
+	call initialize wrt ..plt
+	ret
