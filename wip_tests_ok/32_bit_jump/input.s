be5966e8982238f5d1b25ced2a27803c48991cf5
MyNameIsTrez <welfje@gmail.com>
2024-07-19 21:11:19 +0200

    Pass tests_ok/or_false

---
+++ wip_tests_ok/32_bit_jump/input.s
@@ -0,0 +1,44 @@
+section .data
+
+global define_type
+define_type: db "d", 0
+
+align 8
+global globals_size
+globals_size: dq 0
+
+global on_fns
+on_fns:
+	dq on_a
+
+section .text
+
+extern define_d
+extern initialize
+
+global define
+define:
+	call define_d wrt ..plt
+	ret
+
+global init_globals
+init_globals:
+	ret
+
+global on_a
+on_a:
+	; TODO: Somehow add loads of instructions that will cause the jump to be over 256 bytes long
+	xor eax, eax
+	test rax, rax
+	je $+12
+	mov eax, 1
+	jmp $+18
+	xor eax, eax
+	test rax, rax
+	mov eax, 0
+	setne al
+	push rax
+
+	pop rdi
+	call initialize wrt ..plt
+	ret
