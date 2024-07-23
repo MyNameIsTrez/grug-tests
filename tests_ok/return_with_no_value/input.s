dfaa88363ed8af1631a6013fa9e001dad4e0cdbe
MyNameIsTrez <welfje@gmail.com>
2024-07-23 20:42:28 +0200

    Pass tests_ok/return and tests_ok/return_with_no_value

---
+++ tests_ok/return_with_no_value/input.s
@@ -0,0 +1,38 @@
+section .data
+
+global define_type
+define_type: db "d", 0
+
+align 8
+global globals_size
+globals_size: dq 0
+
+global on_fns
+on_fns:
+	dq on_a
+
+section .text
+
+extern define_d
+extern nothing
+
+global define
+define:
+	call define_d wrt ..plt
+	ret
+
+global init_globals
+init_globals:
+	ret
+
+global on_a
+on_a:
+	call foo
+
+	call nothing wrt ..plt
+	ret
+
+global foo
+foo:
+	ret
+	ret
