95fbede99475bb0a95b2690c2e43391a288131b9
MyNameIsTrez <welfje@gmail.com>
2024-07-23 15:07:45 +0200

    Pass tests_ok/pass_string_argument_to_game_fn

---
+++ tests_ok/pass_string_argument_to_game_fn/input.s
@@ -0,0 +1,39 @@
+section .data
+
+global define_type
+define_type: db "d", 0
+
+align 8
+global globals_size
+globals_size: dq 0
+
+global on_fns
+on_fns:
+	dq on_a
+
+global strings
+strings:
+	db "foo", 0
+
+section .text
+
+extern define_d
+extern say
+
+global define
+define:
+	call define_d wrt ..plt
+	ret
+
+global init_globals
+init_globals:
+	ret
+
+global on_a
+on_a:
+	lea rax, [rel strings+0]
+	push rax
+
+	pop rdi
+	call say wrt ..plt
+	ret
