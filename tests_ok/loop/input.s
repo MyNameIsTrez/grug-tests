9c8deadcae6c3fe4e923f56bfaa4be22eb4f5430
MyNameIsTrez <welfje@gmail.com>
2024-07-23 21:34:01 +0200

    Add tests_ok/loop

---
+++ tests_ok/loop/input.s
@@ -0,0 +1,39 @@
+section .data
+
+global define_type
+define_type: db "d", 0
+
+align 8
+global globals_size
+globals_size: dq 0
+
+global on_fns
+on_fns:
+	dq on_a
+
+section .text
+
+extern define_d
+extern nothing
+
+global define
+define:
+	call define_d wrt ..plt
+	ret
+
+global init_globals
+init_globals:
+	ret
+
+global on_a
+on_a:
+	call nothing wrt ..plt
+
+	mov eax, 1
+	test rax, rax
+	je strict $+0xb
+	call nothing wrt ..plt
+
+	call nothing wrt ..plt
+
+	ret
