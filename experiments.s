e474f8e818f1aea2206afbed2ca2eb68386ef763
MyNameIsTrez <welfje@gmail.com>
2024-08-01 22:45:40 +0200

    Work on passing tests_ok/f32_argument

--- experiments.s
+++ experiments.s
@@ -3,6 +3,27 @@
 global main
 main:
 	; mov rax, 42
+
 	mov eax, 0x3f99999a
+
 	movd xmm0, eax
+	movd xmm1, eax
+	movd xmm2, eax
+	movd xmm3, eax
+	movd xmm4, eax
+	movd xmm5, eax
+	movd xmm6, eax
+	movd xmm7, eax
+
+	movd xmm8, eax
+	movd xmm9, eax
+	movd xmm10, eax
+	movd xmm11, eax
+	movd xmm12, eax
+	movd xmm13, eax
+	movd xmm14, eax
+	movd xmm15, eax
+
+	pop rax
+
 	ret
