54f2ad6e2216b4660826f447b1ffe7e6751e4179
MyNameIsTrez <welfje@gmail.com>
2024-08-02 22:28:43 +0200

    Pass tests_ok/max_args and add tests for too many i32 and f32 arguments

---
+++ tests_ok/max_args/input.s
@@ -0,0 +1,127 @@
+section .data
+
+global define_type
+define_type: db "d", 0
+
+align 8
+global globals_size
+globals_size: dq 0
+
+global on_fns
+on_fns:
+	dq on_a
+
+global strings
+strings:
+	db "foo", 0
+
+section .text
+
+extern game_fn_define_d
+extern game_fn_mega
+
+global define
+define:
+	call game_fn_define_d wrt ..plt
+	ret
+
+global init_globals
+init_globals:
+	ret
+
+global on_a
+on_a:
+    push rbp
+    mov rbp, rsp
+	sub rsp, 0x10
+    mov rbp[-0x8], rdi
+
+	; Pushing arguments
+
+	mov eax, __?float32?__(1.0)
+	push rax
+
+	mov eax, 21
+	push rax
+
+	mov eax, 1
+	push rax
+
+	mov eax, __?float32?__(2.0)
+	push rax
+
+	mov eax, __?float32?__(3.0)
+	push rax
+
+	mov eax, __?float32?__(4.0)
+	push rax
+
+	xor eax, eax
+	push rax
+
+	mov eax, 1337
+	push rax
+
+	mov eax, __?float32?__(5.0)
+	push rax
+
+	mov eax, __?float32?__(6.0)
+	push rax
+
+	mov eax, __?float32?__(7.0)
+	push rax
+
+	mov eax, __?float32?__(8.0)
+	push rax
+
+	mov eax, 8192
+	push rax
+
+	lea rax, strings[rel 0]
+	push rax
+
+	; Popping arguments
+
+	pop r9 ; "foo"
+
+	pop r8 ; 8192
+
+	pop rax ; 8.0
+	movd xmm7, eax
+
+	pop rax ; 7.0
+	movd xmm6, eax
+
+	pop rax ; 6.0
+	movd xmm5, eax
+
+	pop rax ; 5.0
+	movd xmm4, eax
+
+	pop rcx ; 1337
+
+	pop rdx ; false
+
+	pop rax ; 4.0
+	movd xmm3, eax
+
+	pop rax ; 3.0
+	movd xmm2, eax
+
+	pop rax ; 2.0
+	movd xmm1, eax
+
+	pop rsi ; true
+
+	pop rdi ; 21
+
+	pop rax ; 1.0
+	movd xmm0, eax
+
+	; Calling function
+
+	call game_fn_mega wrt ..plt
+
+    mov rsp, rbp
+    pop rbp
+	ret
---
+++ tests_ok/max_args/test.c
@@ -0,0 +1,84 @@
+#include "grug.h"
+
+#include <assert.h>
+#include <dlfcn.h>
+#include <math.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void on_a(void *globals);
+
+struct my_on_fns {
+	typeof(on_a) *a;
+};
+
+void game_fn_define_d(void) {
+}
+
+static bool fn_mega_was_called = false;
+void game_fn_mega(float f1, int32_t i1, bool b1, float f2, float f3, float f4, bool b2, int32_t i2, float f5, float f6, float f7, float f8, int32_t i3, char *str) {
+	fn_mega_was_called = true;
+
+	assert(f1 == 1.0f);
+	assert(i1 == 21);
+	assert(b1 == true);
+	assert(f2 == 2.0f);
+	assert(f3 == 3.0f);
+	assert(f4 == 4.0f);
+	assert(b2 == false);
+	assert(i2 == 1337);
+	assert(f5 == 5.0f);
+	assert(f6 == 6.0f);
+	assert(f7 == 7.0f);
+	assert(f8 == 8.0f);
+	assert(i3 == 8192);
+	assert(strcmp(str, "foo") == 0);
+}
+
+static void *get(void *handle, char *label) {
+	void *p = dlsym(handle, label);
+	if (!p) {
+		fprintf(stderr, "dlsym: %s\n", dlerror());
+		exit(EXIT_FAILURE);
+	}
+	return p;
+}
+
+int main(int argc, char *argv[]) {
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <so_path>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	char *so_path = argv[1];
+
+	void *handle = dlopen(so_path, RTLD_NOW);
+	if (!handle) {
+		fprintf(stderr, "dlopen: %s\n", dlerror());
+		exit(EXIT_FAILURE);
+	}
+
+	assert(strcmp(get(handle, "define_type"), "d") == 0);
+
+	#pragma GCC diagnostic push
+	#pragma GCC diagnostic ignored "-Wpedantic"
+	grug_define_fn_t define = get(handle, "define");
+	define();
+
+	size_t globals_size = *(size_t *)get(handle, "globals_size");
+	assert(globals_size == 0);
+
+	void *g = malloc(globals_size);
+	grug_init_globals_fn_t init_globals = get(handle, "init_globals");
+	init_globals(g);
+
+	struct my_on_fns *on_fns = get(handle, "on_fns");
+	assert(!fn_mega_was_called);
+	on_fns->a(g);
+	assert(fn_mega_was_called);
+
+	free(g);
+	#pragma GCC diagnostic pop
+}
