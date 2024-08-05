78d40a1d6b400faac4bd043d490359328b76a907
MyNameIsTrez <welfje@gmail.com>
2024-08-05 10:14:11 +0200

    Add -DNDEBUG when OPTIMIZED is turned on Let clang instead of gcc also work

--- tests.sh
+++ tests.sh
@@ -119,7 +119,7 @@ run_test_err_runtime() {
 
 		# -std=gnu2x is used to have typeof() in C (-std=c2x for some reason prints "error: expected specifier-qualifier-list before ‘typeof’")
 		# -rdynamic allows the .so to call functions from test.c
-		gcc $test_c_path -Igrug -std=gnu2x -Wall -Wextra -Werror -Wpedantic -Wstrict-prototypes -Wuninitialized -Wfatal-errors -g -Og -rdynamic -o $test_executable_path
+		gcc $test_c_path -Igrug -std=gnu2x -Wall -Wextra -Werror -Wpedantic -Wstrict-prototypes -Wuninitialized -Wfatal-errors -Wno-language-extension-token -g -Og -rdynamic -o $test_executable_path
 	fi
 
 	local grug_output_path=$dir"results/grug_output.txt"
@@ -235,7 +235,7 @@ run_test_ok() {
 
 		# -std=gnu2x is used to have typeof() in C (-std=c2x for some reason prints "error: expected specifier-qualifier-list before ‘typeof’")
 		# -rdynamic allows the .so to call functions from test.c
-		gcc $test_c_path -Igrug -std=gnu2x -Wall -Wextra -Werror -Wpedantic -Wstrict-prototypes -Wuninitialized -Wfatal-errors -g -Og -rdynamic -lm -o $test_executable_path
+		gcc $test_c_path -Igrug -std=gnu2x -Wall -Wextra -Werror -Wpedantic -Wstrict-prototypes -Wuninitialized -Wfatal-errors -Wno-language-extension-token -g -Og -rdynamic -lm -o $test_executable_path
 	fi
 
 	local expected_hex_path=$dir"results/expected.hex"
@@ -328,7 +328,7 @@ init() {
 		if [[ -v OPTIMIZED ]] # If the OPTIMIZED environment variable was set
 		then
 		    echo "- OPTIMIZED was turned on"
-			extra_flags+=' -Ofast -march=native'
+			extra_flags+=' -Ofast -march=native -DNDEBUG'
         else
 			extra_flags+=' -fsanitize=address,undefined -Og'
 		fi
