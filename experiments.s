fe4b2fab8e6d8de50a6c6eb0fa1c2e69d0599d7b
MyNameIsTrez <welfje@gmail.com>
2024-08-07 16:44:04 +0200

    Pass tests_ok/f32_negated

--- experiments.s
+++ experiments.s
@@ -2,13 +2,4 @@
 
 global main
 main:
-	movss rbp[-0x4], xmm0
-	movss rbp[-0x4], xmm1
-	movss rbp[-0x4], xmm2
-	movss rbp[-0x4], xmm3
-	movss rbp[-0x4], xmm4
-	movss rbp[-0x4], xmm5
-	movss rbp[-0x4], xmm6
-	movss rbp[-0x4], xmm7
-
 	ret
