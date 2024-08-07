464b891cc3c03339381feec6b329e648e6f87cef
MyNameIsTrez <welfje@gmail.com>
2024-08-07 15:33:33 +0200

    Test overwriting params

---
+++ tests_ok/helper_fn_overwriting_param/input.s
@@ -0,0 +1,86 @@
+section .data
+
+global define_type
+define_type: db "d", 0
+
+align 8
+global globals_size
+globals_size: dq 0
+
+global on_fns
+on_fns:
+	dq on_a
+
+section .text
+
+extern game_fn_define_d
+extern game_fn_initialize
+extern game_fn_sin
+
+global define
+define:
+	call game_fn_define_d wrt ..plt
+	ret
+
+global init_globals
+init_globals:
+	ret
+
+global on_a
+on_a:
+	push rbp
+	mov rbp, rsp
+	sub rsp, byte 0x10
+    mov rbp[-0x8], rdi
+
+	; helper_foo(2, 3.0)
+	mov rax, rbp[-0x8]
+	push rax
+	mov eax, 2
+	push rax
+	mov eax, __?float32?__(3.0)
+	push rax
+	pop rax
+	movd xmm0, eax
+	pop rsi
+	pop rdi
+	call helper_foo
+
+	mov rsp, rbp
+	pop rbp
+	ret
+
+global helper_foo
+helper_foo:
+	push rbp
+	mov rbp, rsp
+	sub rsp, byte 0x10
+    mov rbp[-0x8], rdi
+	mov rbp[-0xc], esi
+	movss rbp[-0x10], xmm0
+
+	; i = 20
+	mov eax, 20
+	mov rbp[-0xc], eax
+
+	; initialize(i)
+	mov eax, rbp[-0xc]
+	push rax
+	pop rdi
+	call game_fn_initialize wrt ..plt
+
+	; f = 30.0
+	mov eax, __?float32?__(30.0)
+	mov rbp[-0x10], eax
+
+	; sin(f)
+	mov eax, rbp[-0x10]
+	push rax
+	pop rax
+	movd xmm0, eax
+	call game_fn_sin wrt ..plt
+	movd eax, xmm0
+
+	mov rsp, rbp
+	pop rbp
+	ret
---
+++ tests_ok/on_fn_overwriting_param/input.s
@@ -0,0 +1,62 @@
+section .data
+
+global define_type
+define_type: db "s", 0
+
+align 8
+global globals_size
+globals_size: dq 0
+
+global on_fns
+on_fns:
+	dq on_a
+
+section .text
+
+extern game_fn_define_s
+extern game_fn_initialize
+extern game_fn_sin
+
+global define
+define:
+	call game_fn_define_s wrt ..plt
+	ret
+
+global init_globals
+init_globals:
+	ret
+
+global on_a
+on_a:
+	push rbp
+	mov rbp, rsp
+	sub rsp, byte 0x10
+    mov rbp[-0x8], rdi
+	mov rbp[-0xc], esi
+	movss rbp[-0x10], xmm0
+
+	; i = 20
+	mov eax, 20
+	mov rbp[-0xc], eax
+
+	; initialize(i)
+	mov eax, rbp[-0xc]
+	push rax
+	pop rdi
+	call game_fn_initialize wrt ..plt
+
+	; f = 30.0
+	mov eax, __?float32?__(30.0)
+	mov rbp[-0x10], eax
+
+	; sin(f)
+	mov eax, rbp[-0x10]
+	push rax
+	pop rax
+	movd xmm0, eax
+	call game_fn_sin wrt ..plt
+	movd eax, xmm0
+
+	mov rsp, rbp
+	pop rbp
+	ret
