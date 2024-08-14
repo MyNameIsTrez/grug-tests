da607dbfb4ffb85fc1dff3e4027b5eea0d888a2f
MyNameIsTrez <welfje@gmail.com>
2024-08-14 11:18:43 +0200

    Add tests_ok/variable_string_local and tests_ok/variable_string_global

---
+++ tests_ok/variable_string_global/input.s
@@ -0,0 +1,55 @@
+section .data
+
+global define_type
+define_type: db "d", 0
+
+align 8
+global globals_size
+globals_size: dq 8
+
+global on_fns
+on_fns:
+	dq on_a
+
+global strings
+strings:
+	db "temp", 0
+	db "foo", 0
+
+section .text
+
+extern game_fn_define_d
+extern game_fn_say
+
+global define
+define:
+	call game_fn_define_d wrt ..plt
+	ret
+
+global init_globals
+init_globals:
+	lea rax, strings[rel 0]
+	mov rdi[byte 0x0], rax
+	ret
+
+global on_a
+on_a:
+	push rbp
+	mov rbp, rsp
+	sub rsp, byte 0x10
+    mov rbp[-0x8], rdi
+
+	lea rax, strings[rel 5]
+	mov r11, rbp[-0x8]
+	mov r11[byte 0], rax
+
+	mov rax, rbp[-0x8]
+	mov rax, rax[byte 0x0]
+	push rax
+
+	pop rdi
+	call game_fn_say wrt ..plt
+
+	mov rsp, rbp
+	pop rbp
+	ret
