27e9e0824f8fe1fbccaa51542954312c42821d2b
MyNameIsTrez <welfje@gmail.com>
2024-08-20 23:09:26 +0200

    Add tests_ok/blocked_alrm

---
+++ tests_ok/blocked_alrm/input.s
@@ -0,0 +1,77 @@
+section .data
+
+global define_type
+define_type: db "d", 0
+
+align 8
+global globals_size
+globals_size: dq 0
+
+global on_fns
+on_fns:
+	dq on_a
+
+section .text
+
+extern game_fn_define_d
+extern grug_enable_on_fn_runtime_error_handling
+extern game_fn_nothing
+extern grug_disable_on_fn_runtime_error_handling
+extern sigprocmask
+extern grug_block_alrm_mask
+extern _GLOBAL_OFFSET_TABLE_
+
+; TODO: Use?
+; From https://www.nasm.us/xdoc/2.15/html/nasmdo10.html
+; %macro get_GOT 0
+; 	call %%getgot
+; %%getgot:
+; 	pop rbx
+; 	add rbx, _GLOBAL_OFFSET_TABLE_ + $$ - %%getgot wrt ..gotpc
+; %endmacro
+
+global define
+define:
+	call game_fn_define_d wrt ..plt
+	ret
+
+global init_globals
+init_globals:
+	ret
+
+global on_a
+on_a:
+	push rbp
+	mov rbp, rsp
+	sub rsp, byte 0x10
+	mov rbp[-0x8], rdi
+
+	; From https://www.nasm.us/xdoc/2.15/html/nasmdo10.html
+	push rbx
+	; get_GOT
+	call .get_GOT ; TODO: Try doing smth like `call $+42` instead, so that the output doesn't have the `on_a.get_GOT` label
+.get_GOT:
+	pop rbx
+	add rbx, _GLOBAL_OFFSET_TABLE_ + $$ - .get_GOT wrt ..gotpc
+
+	call grug_enable_on_fn_runtime_error_handling wrt ..plt
+
+	xor edi, edi
+	mov rsi, [rbx + grug_block_alrm_mask wrt ..got]
+	xor edx, edx
+	call sigprocmask wrt ..plt
+
+	call game_fn_nothing wrt ..plt
+
+	mov edi, 1
+	mov rsi, [rbx + grug_block_alrm_mask wrt ..got]
+	xor edx, edx
+	call sigprocmask wrt ..plt
+
+	call grug_disable_on_fn_runtime_error_handling wrt ..plt
+
+	mov rbx, rbp[-0x8] ; TODO: It is -0x4 in https://www.nasm.us/xdoc/2.15/html/nasmdo10.html
+
+	mov rsp, rbp
+	pop rbp
+	ret
---
+++ tests_ok/blocked_alrm/test.c
@@ -0,0 +1,73 @@
+#define _XOPEN_SOURCE 700 // This is just so VS Code can find sigset_t
+
+#include "grug.h"
+
+#include "mod_api.h"
+
+#include <assert.h>
+#include <dlfcn.h>
+#include <signal.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void game_fn_define_d(void) {
+}
+
+static bool fn_nothing_was_called = false;
+void game_fn_nothing(void) {
+	fn_nothing_was_called = true;
+
+	sigset_t mask;
+
+	sigprocmask(SIG_BLOCK, NULL, &mask);
+
+	assert(sigismember(&mask, SIGALRM));
+}
+
+static void *get(void *handle, char *label) {
+	void *p = dlsym(handle, label);
+	if (!p) {
+		fprintf(stderr, "dlsym: %s\n", dlerror());
+		exit(EXIT_FAILURE);
+	}
+	return p;
+}
+
+int main(int argc, char *argv[]) {
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <so_path>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	char *so_path = argv[1];
+
+	void *handle = dlopen(so_path, RTLD_NOW);
+	if (!handle) {
+		fprintf(stderr, "dlopen: %s\n", dlerror());
+		exit(EXIT_FAILURE);
+	}
+
+	assert(strcmp(get(handle, "define_type"), "d") == 0);
+
+	#pragma GCC diagnostic push
+	#pragma GCC diagnostic ignored "-Wpedantic"
+	grug_define_fn_t define = get(handle, "define");
+	define();
+
+	size_t globals_size = *(size_t *)get(handle, "globals_size");
+	assert(globals_size == 0);
+
+	void *g = malloc(globals_size);
+	grug_init_globals_fn_t init_globals = get(handle, "init_globals");
+	init_globals(g);
+
+	struct d_on_fns *on_fns = get(handle, "on_fns");
+	assert(!fn_nothing_was_called);
+	on_fns->a(g);
+	assert(fn_nothing_was_called);
+
+	free(g);
+	#pragma GCC diagnostic pop
+}
