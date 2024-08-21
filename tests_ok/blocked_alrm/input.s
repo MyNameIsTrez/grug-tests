9a71ecec893db92c2ac61288307b8840130ba2d6
MyNameIsTrez <welfje@gmail.com>
2024-08-21 18:36:26 +0200

    Working on passing tests_ok/block_alrm

--- tests_ok/blocked_alrm/input.s
+++ tests_ok/blocked_alrm/input.s
@@ -13,12 +13,13 @@ on_fns:
 
 section .text
 
+extern grug_block_mask
+
 extern game_fn_define_d
 extern grug_enable_on_fn_runtime_error_handling
+extern sigprocmask
 extern game_fn_nothing
 extern grug_disable_on_fn_runtime_error_handling
-extern sigprocmask
-extern grug_block_alrm_mask
 extern _GLOBAL_OFFSET_TABLE_
 
 global define
@@ -34,32 +35,31 @@ global on_a
 on_a:
 	push rbp
 	mov rbp, rsp
-	sub rsp, byte 0x10
+	sub rsp, byte 0x18
 	mov rbp[-0x8], rdi
 
 	; From https://www.nasm.us/xdoc/2.15/html/nasmdo10.html
 	push rbx
-	lea rbx, [rel .get_GOT]
-.get_GOT:
-	add rbx, _GLOBAL_OFFSET_TABLE_ + $$ - .get_GOT wrt ..gotpc
+	lea rbx, [rel $$]
+	add rbx, _GLOBAL_OFFSET_TABLE_ wrt ..gotpc
 
 	call grug_enable_on_fn_runtime_error_handling wrt ..plt
 
 	xor edi, edi
-	mov rsi, [rbx + grug_block_alrm_mask wrt ..got]
+	mov rsi, [rbx + grug_block_mask wrt ..got]
 	xor edx, edx
 	call sigprocmask wrt ..plt
 
 	call game_fn_nothing wrt ..plt
 
 	mov edi, 1
-	mov rsi, [rbx + grug_block_alrm_mask wrt ..got]
+	mov rsi, [rbx + grug_block_mask wrt ..got]
 	xor edx, edx
 	call sigprocmask wrt ..plt
 
 	call grug_disable_on_fn_runtime_error_handling wrt ..plt
 
-	mov rbx, rbp[-0x8] ; TODO: It is -0x4 in https://www.nasm.us/xdoc/2.15/html/nasmdo10.html
+	mov rbx, rbp[-0x8]
 
 	mov rsp, rbp
 	pop rbp
