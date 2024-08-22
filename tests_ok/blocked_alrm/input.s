762a9d94292f8a674f4841735aa5540c2a3b6408
MyNameIsTrez <welfje@gmail.com>
2024-08-22 22:36:49 +0200

    Pass tests_ok/blocked_alrm

--- tests_ok/blocked_alrm/input.s
+++ tests_ok/blocked_alrm/input.s
@@ -35,35 +35,34 @@ global on_a
 on_a:
 	push rbp
 	mov rbp, rsp
-	sub rsp, byte 0x18
-	mov rbp[-0x8], rdi
+	sub rsp, byte 0x10
+	mov rbp[-0x8], rbx
+	mov rbp[-0x10], rdi
 
 	; From https://www.nasm.us/xdoc/2.15/html/nasmdo10.html
-	; We want rbx since it's a callee-saved register,
-	; meaning game fn calls can't overwrite it, meaning we don't have to
+	; We want rbx, since it's a callee-saved register.
+	; Game fn calls can't overwrite it, meaning we don't have to
 	; reload the global offset table every time we want to access a global
-	push rbx
 	lea rbx, [rel $$]
 	add rbx, _GLOBAL_OFFSET_TABLE_ wrt ..gotpc
 
 	call grug_enable_on_fn_runtime_error_handling wrt ..plt
 
-	xor edi, edi
-	mov rsi, [rbx + grug_block_mask wrt ..got]
 	xor edx, edx
+	mov rsi, rbx[grug_block_mask wrt ..got]
+	xor edi, edi
 	call sigprocmask wrt ..plt
 
 	call game_fn_nothing wrt ..plt
 
-	mov edi, 1
-	mov rsi, [rbx + grug_block_mask wrt ..got]
 	xor edx, edx
+	mov rsi, rbx[grug_block_mask wrt ..got]
+	mov edi, 1
 	call sigprocmask wrt ..plt
 
 	call grug_disable_on_fn_runtime_error_handling wrt ..plt
 
 	mov rbx, rbp[-0x8]
-
 	mov rsp, rbp
 	pop rbp
 	ret
