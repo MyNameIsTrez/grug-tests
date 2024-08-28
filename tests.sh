446d12c002fd6cc61a02956cad288120509f7d01
MyNameIsTrez <welfje@gmail.com>
2024-08-28 15:14:35 +0200

    Check for errors in tests.sh

--- tests.sh
+++ tests.sh
@@ -11,13 +11,13 @@ linker_flags='-rdynamic'
 if (! [[ grug/grug.c -ot tests.out ]]) || (! [[ grug/grug.h -ot tests.out ]]) || (! [[ tests.sh -ot tests.out ]])
 then
     echo "Recompiling grug.o..."
-    clang grug/grug.c -c -o grug.o $compiler_flags
+    clang grug/grug.c -c -o grug.o $compiler_flags || { echo 'Recompiling grug.o failed :('; exit 1; }
 fi
 
 if (! [[ tests.c -ot tests.o ]]) || (! [[ tests.sh -ot tests.out ]])
 then
     echo "Recompiling tests.o..."
-    clang tests.c -c -o tests.o $compiler_flags
+    clang tests.c -c -o tests.o $compiler_flags || { echo 'Recompiling tests.o failed :('; exit 1; }
 fi
 
 if (! [[ tests.o -ot tests.out ]]) || (! [[ grug.o -ot tests.out ]]) || (! [[ tests.sh -ot tests.out ]])
@@ -25,7 +25,7 @@ then
     # TODO: Try using the mold linker here, and add README instructions back if it's faster
     echo "Linking tests.out..."
     # TODO: Try removing $compiler_flags here
-    clang tests.o grug.o -o tests.out $compiler_flags $linker_flags
+    clang tests.o grug.o -o tests.out $compiler_flags $linker_flags || { echo 'Linking tests.out failed :('; exit 1; }
 fi
 
 echo "Running tests.out..."
