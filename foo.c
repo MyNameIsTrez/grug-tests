039e2df2e16e35ffee214d9fd91e73017bef74b8
MyNameIsTrez <welfje@gmail.com>
2024-09-13 20:15:43 +0200

    Print the current architecture

--- foo.c
+++ foo.c
@@ -5,6 +5,14 @@
 int main(void) {
 	int64_t rsp;
 
+#ifdef __x86_64__
+	printf("__x86_64__\n");
+#elif __i386__
+	printf("__i386__\n");
+#elif __arm__
+	printf("__arm__\n");
+#endif
+
 	#pragma GCC diagnostic push
 	#pragma GCC diagnostic ignored "-Wlanguage-extension-token"
 
