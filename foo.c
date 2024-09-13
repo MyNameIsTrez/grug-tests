f949ae9788829010e307ca8a91b5dd5e28e617d2
MyNameIsTrez <welfje@gmail.com>
2024-09-13 21:42:27 +0200

    Swap `sp` and `%0`

--- foo.c
+++ foo.c
@@ -5,8 +5,8 @@ int printf(const char *restrict format, ...);
 int main(void) {
 	int64_t rsp;
 
-	asm("mov sp, %0\n\t" : "=r" (rsp));
-	// asm("mov %0, sp\n\t" : "=r" (rsp));
+	// asm("mov sp, %0\n\t" : "=r" (rsp));
+	asm("mov %0, sp\n\t" : "=r" (rsp));
 
     printf("%lld\n", rsp);
 }
