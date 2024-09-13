8f71eec9a0571a2a0f4d257014961fe99b1f7322
MyNameIsTrez <welfje@gmail.com>
2024-09-13 20:18:21 +0200

    Try using the ARM sp register

--- foo.c
+++ foo.c
@@ -3,23 +3,23 @@
 #include <stdlib.h>
 
 int main(void) {
-	// int64_t rsp;
+	int64_t rsp;
 
-#ifdef __x86_64__
-	fprintf(stderr, "__x86_64__\n");
-#elif __i386__
-	fprintf(stderr, "__i386__\n");
-#elif __arm__
-	fprintf(stderr, "__arm__\n");
-#endif
+// #ifdef __x86_64__
+// 	fprintf(stderr, "__x86_64__\n");
+// #elif __i386__
+// 	fprintf(stderr, "__i386__\n");
+// #elif __arm__
+// 	fprintf(stderr, "__arm__\n");
+// #endif
 
-	// #pragma GCC diagnostic push
-	// #pragma GCC diagnostic ignored "-Wlanguage-extension-token"
+	#pragma GCC diagnostic push
+	#pragma GCC diagnostic ignored "-Wlanguage-extension-token"
 
-	// // asm("mov %%rsp, %0" : "=r" (rsp));
-	// asm("movq %%rsp, %0" : "=r" (rsp));
+	// asm("mov %%rsp, %0" : "=r" (rsp));
+	asm("mov %%sp, %0" : "=r" (rsp));
 
-	// #pragma GCC diagnostic pop
+	#pragma GCC diagnostic pop
 
-    // fprintf(stderr, "%lld\n", rsp);
+    fprintf(stderr, "%lld\n", rsp);
 }
