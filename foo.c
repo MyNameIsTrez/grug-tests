159ebb79bddd84010a4dd36c698914eafdf0a15b
MyNameIsTrez <welfje@gmail.com>
2024-09-13 21:36:21 +0200

    Compiling to aarch64 locally working

--- foo.c
+++ foo.c
@@ -1,54 +1,11 @@
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+typedef long long int64_t;
+
+int printf(const char *restrict format, ...);
 
 int main(void) {
 	int64_t rsp;
 
-// #ifdef __x86_64__
-// 	printf("__x86_64__\n");
-// #elif _M_X64
-// 	printf("_M_X64\n");
-// #elif __i386__
-// 	printf("__i386__\n");
-// #elif _M_IX86
-// 	printf("_M_IX86\n");
-// // #elif __aarch64__
-// // 	printf("__aarch64__\n");
-// #elif _M_ARM64
-// 	printf("_M_ARM64\n");
-// #elif __arm__
-// 	printf("__arm__\n");
-// #elif _M_ARM
-// 	printf("_M_ARM\n");
-// #elif __EMSCRIPTEN__
-// 	printf("__EMSCRIPTEN__\n");
-// #else
-// 	printf("none!\n");
-// #endif
-
-	// #pragma GCC diagnostic push
-	// #pragma GCC diagnostic ignored "-Wlanguage-extension-token"
-
-	// asm("mov %%rsp, %0" : "=r" (rsp));
-	// asm("mov %%sp, %0" : "=r" (rsp));
-
-    // __asm__ __volatile__(
-    //     // sp is an alias for r13
-    //     "mov %%sp,%0"
-    //     : "=r" (rsp)
-    //     : /* no input */
-    // );
-
-    __asm__ __volatile__(
-        // sp is an alias for r13
-        "movq %%rsp,%0"
-        : "=r" (rsp)
-        : /* no input */
-    );
-
-	// #pragma GCC diagnostic pop
+	asm("mov sp, %0" : "=r" (rsp));
 
     printf("%lld\n", rsp);
 }
