ae8edaa32d8bbdefec47a42c28c7241dcd50a6fa
MyNameIsTrez <welfje@gmail.com>
2024-09-13 21:13:03 +0200

    Add more defines

--- foo.c
+++ foo.c
@@ -6,12 +6,24 @@
 int main(void) {
 	// int64_t rsp;
 
-#ifdef defined( __x86_64__ )
+#ifdef __x86_64__
 	printf("__x86_64__\n");
-#elif defined( __i386__ )
+#elif _M_X64
+	printf("_M_X64\n");
+#elif __i386__
 	printf("__i386__\n");
-#elif defined( __arm__ )
+#elif _M_IX86
+	printf("_M_IX86\n");
+#elif __aarch64__
+	printf("__aarch64__\n");
+#elif _M_ARM64
+	printf("_M_ARM64\n");
+#elif __arm__
 	printf("__arm__\n");
+#elif _M_ARM
+	printf("_M_ARM\n");
+#elif __EMSCRIPTEN__
+	printf("__EMSCRIPTEN__\n");
 #else
 	printf("none!\n"); // TODO: WHY DOES THIS END UP GETTING PRINTED??
 #endif
