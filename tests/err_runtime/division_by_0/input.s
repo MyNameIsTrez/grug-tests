6a3fd14d613bd5b5bb0309793735b49217a64847
MyNameIsTrez <welfje@gmail.com>
2024-11-10 00:49:41 +0100

    Update division_by_0 to have the fast implementation of its on fn

--- tests/err_runtime/division_by_0/input.s
+++ tests/err_runtime/division_by_0/input.s
@@ -27,6 +27,7 @@ section .text
 
 extern grug_runtime_error_handler
 extern grug_runtime_error_jmp_buffer
+extern grug_on_fns_in_safe_mode
 extern grug_block_mask
 extern grug_runtime_error_type
 extern game_fn_define_d
@@ -96,6 +97,11 @@ on_a:
 	sub rsp, byte 0x10
 	mov rbp[-0x8], rdi
 
+	mov rax, [rel grug_on_fns_in_safe_mode wrt ..got]
+	mov al, [rax]
+	test al, al
+	je strict $+0x9e
+
 	error_handling
 
 	call grug_enable_on_fn_runtime_error_handling wrt ..plt
@@ -114,6 +120,21 @@ on_a:
 	unblock
 
 	call grug_disable_on_fn_runtime_error_handling wrt ..plt
+
+	mov rsp, rbp
+	pop rbp
+	ret
+
+	xor eax, eax
+	push rax
+	mov eax, 1
+	pop r11
+	cqo
+	idiv r11
+	push rax
+
+	pop rdi
+	call game_fn_initialize wrt ..plt
 
 	mov rsp, rbp
 	pop rbp
