d6f07fc292b2d3eba2d6740aae9e9e9a850d80ae
Sander Bos <32989873+MyNameIsTrez@users.noreply.github.com>
2025-01-10 10:16:26 +0100

    Move the pragma pop closer

--- tests.c
+++ tests.c
@@ -31,13 +31,13 @@ bool grug_test_regenerate_dll(char *grug_file_path, char *dll_path, char *mod);
 	\
 	asm("mov %%rsp, %0\n\t" : "=r" (rsp));\
 	\
+	_Pragma("GCC diagnostic pop")\
+	\
 	if ((rsp & 0xf) != 0) {\
 		static char msg[] = "The stack was not 16-byte aligned!\n";\
 		write(STDERR_FILENO, msg, sizeof(msg) - 1);\
 		abort();\
 	}\
-	\
-	_Pragma("GCC diagnostic pop")\
 }
 #elif __aarch64__
 #define ASSERT_16_BYTE_STACK_ALIGNED() {\
@@ -48,13 +48,13 @@ bool grug_test_regenerate_dll(char *grug_file_path, char *dll_path, char *mod);
 	\
 	asm("mov %0, sp\n\t" : "=r" (rsp));\
 	\
+	_Pragma("GCC diagnostic pop")\
+	\
 	if ((rsp & 0xf) != 0) {\
 		static char msg[] = "The stack was not 16-byte aligned!\n";\
 		write(STDERR_FILENO, msg, sizeof(msg) - 1);\
 		abort();\
 	}\
-	\
-	_Pragma("GCC diagnostic pop")\
 }
 #else
 #error Unsupported or unrecognized architecture
