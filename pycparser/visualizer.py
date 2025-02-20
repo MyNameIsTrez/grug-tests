e3cb14e3c28dee1afcbc4afbfc70864ebafa88b3
Sander Bos <sander@streamhpc.com>
2025-02-20 13:57:09 +0100

    More visualizer.py experiments

--- pycparser/visualizer.py
+++ pycparser/visualizer.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+from networkx.drawing.nx_agraph import graphviz_layout
+import networkx as nx
 import sys
 
 # This is not required if you've installed pycparser into
@@ -46,7 +49,57 @@ def run():
     v = FuncDefVisitor()
     v.visit(ast)
 
-    print(graph)
+    G = nx.DiGraph()
+    labels = {}
+    for definition, calls in graph.items():
+        labels[definition] = definition
+
+        for call in calls:
+            label_key = definition + "_calls_" + call
+            labels[label_key] = call
+
+            print(definition, call)
+            # G.add_edge(definition, call)
+            G.add_edge(definition, label_key)
+    print(labels)
+    # G.add_edge(0, 1)
+    # G.add_edge(0, 2)
+    # G.add_edges_from([(0, 1), (0, 2)])
+    # pos = nx.spring_layout(G)
+    # pos = graphviz_layout(G, prog='dot', root=0, args='-Grankdir="LR"')
+    pos = graphviz_layout(G, prog='dot')
+    print(pos)
+
+    plt.axis('off')
+    fig = plt.figure(1)
+    # fig.set_figwidth(100.0)
+    # fig.set_figheight(100.0)
+
+    nx.draw_networkx_nodes(G, pos, node_shape='o', node_size=1000)
+    nx.draw_networkx_edges(G, pos, edgelist=G.edges(), arrows=True)
+    # nx.draw_networkx_labels(G, pos, font_size=10, font_color="white")
+    # TODO: The reason no line is drawn between main() and foo(), is because foo() its key is "main_calls_foo"
+    nx.draw_networkx_labels(G, pos, labels, font_size=10, font_color="white")
+    plt.savefig("graph.png")
+
+
+    # plt.figure()
+    # plt.axis('off')
+    # fig = plt.figure(1)
+    # pos = nx.spring_layout(G)
+    # # pos = graphviz_layout(G, prog='dot')
+    # nx.draw_networkx_nodes(G, pos)
+    # nx.draw_networkx_edges(G, pos)
+    # nx.draw_networkx_labels(G, pos)
+
+    # # plt.xlim(min(x for x, y in pos.values()), max(x for x, y in pos.values()))
+    # # plt.ylim(min(y for x, y in pos.values()), max(y for x, y in pos.values()))
+
+    # fig.set_figwidth(100.0)
+    # fig.set_figheight(100.0)
+
+    # plt.savefig("graph.png",bbox_inches="tight")
+    # plt.close()
 
 
 if __name__ == "__main__":
